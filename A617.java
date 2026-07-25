9dc0c2de9b0d0424b1a5a223c93ca52d572e5d83
Yash5535x <yash5535x@gmail.com>
2026-07-25 12:26:41 +0530

    soloved 617A

---
+++ A617.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class A617 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        int o = ((x+4)/5);
+        System.out.println(o);
+    }
+}
