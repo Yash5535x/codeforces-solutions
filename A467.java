f3d9ece5dd3d3fcf236ae81cd9f58e22c7cf0321
Yash5535x <yash5535x@gmail.com>
2026-08-09 11:39:27 +0530

    solved 467A in java

---
+++ A467.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class A467 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        
+        for (int i = 0; i < n; i++) {
+            int p = sc.nextInt();
+            int q = sc.nextInt();
+            if (q - p >= 2) {
+                count++;
+            }
+        }
+        
+        System.out.println(count);
+    }
+}
