636f6fac2d793b1fea4d6287f833b236f3639652
Yash5535x <yash5535x@gmail.com>
2026-08-10 17:09:41 +0530

    solved 158A in java

---
+++ A158.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class A158 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] scores = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            scores[i] = sc.nextInt();
+        }
+        
+        int cutoff = scores[k-1];
+        int count = 0;
+        for (int x : scores) {
+            if (x >= cutoff && x > 0) {
+                count++;
+            }
+        }
+        
+        System.out.println(count);
+    }
+}
