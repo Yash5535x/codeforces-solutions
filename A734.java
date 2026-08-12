b7388aca45bda541bd17111731a48201bd1d103f
Yash5535x <yash5535x@gmail.com>
2026-08-12 22:01:37 +0530

    solved 734A in java

---
+++ A734.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class A734 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = sc.next();
+        
+        int anton = 0, danik = 0;
+        
+        for (char c : s.toCharArray()) {
+            if (c == 'A') anton++;
+            else danik++;
+        }
+        
+        if (anton > danik) {
+            System.out.println("Anton");
+        } else if (danik > anton) {
+            System.out.println("Danik");
+        } else {
+            System.out.println("Friendship");
+        }
+    }
+}
