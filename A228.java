dfdc655ff2cf15d948833c755a9ece8a17492ffd
Yash5535x <yash5535x@gmail.com>
2026-08-14 21:55:14 +0530

    solved 228A in java

---
+++ A228.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class A228 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Set<Integer> set = new HashSet<>();
+        
+        for (int i = 0; i < 4; i++) {
+            set.add(sc.nextInt());
+        }
+        
+        System.out.println(4 - set.size());
+    }
+}
