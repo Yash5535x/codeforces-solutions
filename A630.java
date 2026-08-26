904fd4e7ae279aae2623374b8ce188545a55946b
Yash5535x <yash5535x@gmail.com>
2026-08-26 21:42:04 +0530

    solved 630A in java

---
+++ A630.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class A630{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        long s = sc.nextLong();
+        if(s==1){
+            System.out.print(05);
+        }else{
+            System.out.print(25);
+        }
+
+    } 
+}
