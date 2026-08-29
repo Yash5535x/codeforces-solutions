c2afc2af06501c4fbc6d55c2d5356c34d4105eba
Yash5535x <yash5535x@gmail.com>
2026-08-29 10:32:27 +0530

    solved 1335A in java

---
+++ A1335.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class A1335{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int o = sc.nextInt();
+        
+        for(int i=0; i<o ; i++){
+            int c = sc.nextInt();
+            int s = (c-1) /2;
+            System.out.println(s);
+        }
+
+    }
+}
