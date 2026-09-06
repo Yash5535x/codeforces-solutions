016712edd76b50383e76b8eb49964f3a762e6f2a
Yash5535x <yash5535x@gmail.com>
2026-09-06 22:13:41 +0530

    solved 1915A in java

---
+++ A1915.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class A1915{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n  = sc.nextInt();
+
+        for(int i =0; i<n ; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            if(a==b){
+                System.out.println(c);
+            }else if(a==c){
+                System.out.println(b);
+            }
+            else{
+                System.out.println(a);
+            }
+        }
+    }
+} 
